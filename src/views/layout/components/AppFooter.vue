bf9f632e978d393bf575766b827788cd2a069de9
ilovezy <1518265554@qq.com>
2019-02-20 10:10:59 +0800

    a

--- src/views/layout/components/AppFooter.vue
+++ src/views/layout/components/AppFooter.vue
@@ -1,6 +1,8 @@
 <template>
   <div class='footer'>
      <div class='footer-wrap global-container'>
+       <span style='margin-right: 30px;'>浙ICP备19001732号</span>
+       <span style='margin-right: 30px;'>浙网文 [2019] 0653-054号</span>
        <span>Copyright © 杭州久和星途网络科技有限公司</span>
        <!--<span style='padding-left: 50px;'>联系方式：18768143328</span>-->
        <!--<span style='padding-left: 50px;'>qq：1518265554</span>-->
